1dab0fc1c56fb5ccde5ecb380de8757034e81ca9
Tien <tranminhtien31166@gmail.com>
2022-12-09 23:04:34 +0700

    [UPDATE] add drap drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {BackendService} from './backend.service';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,22 @@ export class AppComponent {
   tasks = this.backend.tasks();
   users = this.backend.users();
 
+  public todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  public done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
   constructor(private backend: BackendService) {}
+
+  public  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {BackendService} from './backend.service';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule
+  ],
+  exports: [
+    DragDropModule
   ],
   providers: [BackendService],
   bootstrap: [AppComponent]
