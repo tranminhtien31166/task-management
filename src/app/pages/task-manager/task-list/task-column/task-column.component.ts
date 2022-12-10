b7e374029deccb95f3d0d7b5c53be1f64d8abbe8
Tien <tranminhtien31166@gmail.com>
2022-12-11 01:31:53 +0700

    [update] function task column

--- src/app/pages/task-manager/task-list/task-column/task-column.component.ts
+++ src/app/pages/task-manager/task-list/task-column/task-column.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from "@angular/cdk/drag-drop";
+import { ModelTaskCategory } from "@app/constant";
 
 @Component({
-  selector: 'app-task-column',
-  templateUrl: './task-column.component.html',
-  styleUrls: ['./task-column.component.css']
+  selector: "app-task-column",
+  templateUrl: "./task-column.component.html",
+  styleUrls: ["./task-column.component.css"],
 })
 export class TaskColumnComponent implements OnInit {
+  @Input() category: ModelTaskCategory;
+  public todo = ["Get to work", "Pick up groceries", "Go home", "Fall asleep"];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  public drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
   }
-
 }
--- src/app/pages/task-manager/task-list/task-list.component.ts
+++ src/app/pages/task-manager/task-list/task-list.component.ts
@@ -1,34 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 // import {BackendService} from './backend.service';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from "@angular/cdk/drag-drop";
+import { ModelTaskCategory } from "@app/constant";
 
 @Component({
-  selector: 'app-task-list',
-  templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
+  selector: "app-task-list",
+  templateUrl: "./task-list.component.html",
+  styleUrls: ["./task-list.component.css"],
 })
 export class TaskListComponent implements OnInit {
-  // tasks = this.backend.tasks();
-  // users = this.backend.users();
+  public taskCategory: any[] = [
+    {
+      id: 0,
+      name: "Draf",
+      color: "#adb5bd",
+      cards: [],
+    },
+    {
+      id: 1,
+      name: "Doing",
+      color: "#c2d3fb",
+      cards: [],
+    },
+    {
+      id: 2,
+      name: "Review",
+      color: "#fbe3c4",
+      cards: [],
+    },
+    {
+      id: 3,
+      name: "Done",
+      color: "#d4f3db",
+      cards: [],
+    },
+  ];
 
-  public todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  constructor() {}
 
-  public done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  public  drop(event: CdkDragDrop<string[]>) {
+  public drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
     }
   }
