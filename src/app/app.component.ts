694e64fbc9622d453e9417fa5a0f6ad7faaf3fa9
Tien <tranminhtien31166@gmail.com>
2022-12-11 00:11:02 +0700

    [update] small fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {BackendService} from './backend.service';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 
 @Component({
@@ -9,25 +8,6 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tasks = this.backend.tasks();
-  users = this.backend.users();
-
-  public todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  public done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
   constructor(private backend: BackendService) {}
 
-  public  drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BackendService} from './backend.service';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import { BackendService} from './backend.service';
 import { LoginComponent } from './pages/login/login.component';
 import { HeaderComponent } from '@app/layouts/header/header.component';
 import { AdminComponent } from '@app/layouts/admin.component';
@@ -19,10 +18,9 @@ import { AdminComponent } from '@app/layouts/admin.component';
     BrowserModule,
     AppRoutingModule,
   ],
-  exports: [
-    DragDropModule,
+  providers: [
+  	BackendService
   ],
-  providers: [BackendService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
