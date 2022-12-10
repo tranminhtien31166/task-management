dc225091319e61364b07f28996c06f83566b2340
Tien <tranminhtien31166@gmail.com>
2022-12-10 13:48:52 +0700

    [UPDATE] add component task-manager

---
+++ src/app/pages/task-manager/task-manager-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@app/helpers';
+import { TaskListComponent } from './task-list/task-list.component';
+
+const routes: Routes = [
+  { path: 'list', component: TaskListComponent, canActivate: [AuthGuard] },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TaskManagerRoutingModule { }
---
+++ src/app/pages/task-manager/task-manager.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TaskManagerRoutingModule } from './task-manager-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TaskManagerRoutingModule
+  ]
+})
+export class TaskManagerModule { }
