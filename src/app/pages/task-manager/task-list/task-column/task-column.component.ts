869d629880f2103da816811283636a57316cb217
Tien <tranminhtien31166@gmail.com>
2022-12-10 23:26:30 +0700

    [update] add component task

---
+++ src/app/pages/task-manager/task-list/task-column/task-column.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-column',
+  templateUrl: './task-column.component.html',
+  styleUrls: ['./task-column.component.css']
+})
+export class TaskColumnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
