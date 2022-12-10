2e7fffd2d3a65c88e370fecff6416847d33195cc
Tien <tranminhtien31166@gmail.com>
2022-12-11 00:11:51 +0700

    [update] add drap drop

--- src/app/pages/task-manager/task-manager.module.ts
+++ src/app/pages/task-manager/task-manager.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskManagerRoutingModule } from './task-manager-routing.module';
+import { TaskColumnComponent } from './task-list/task-column/task-column.component';
+import { TaskCardComponent } from './task-list/task-card/task-card.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TaskListComponent,
+    TaskColumnComponent,
+    TaskCardComponent
+  ],
   imports: [
     CommonModule,
     TaskManagerRoutingModule
-  ]
+  ],
+  exports: [
+    DragDropModule,
+  ],
+  // schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class TaskManagerModule { }
