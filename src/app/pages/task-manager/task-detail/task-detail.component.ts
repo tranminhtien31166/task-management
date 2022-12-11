2f27b2af995df1cd71c4007898105adc38d39da3
Tien <tranminhtien31166@gmail.com>
2022-12-11 11:13:54 +0700

    [update] form add task

--- src/app/pages/task-manager/task-detail/task-detail.component.ts
+++ src/app/pages/task-manager/task-detail/task-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-task-detail',
@@ -7,13 +7,20 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./task-detail.component.css']
 })
 export class TaskDetailComponent implements OnInit {
-
+  public
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data
+    @Inject(MAT_DIALOG_DATA) public data,
+    public dialogRef: MatDialogRef<TaskDetailComponent>,
   ) { }
 
   ngOnInit(): void {
     console.log(this.data);
+  }
+  public closeFormDialog(close: any) {
+    this.dialogRef.close(close);
+  }
+  public handleChangeInput(event) {
+    console.log(event.target.value);
   }
 
 }
