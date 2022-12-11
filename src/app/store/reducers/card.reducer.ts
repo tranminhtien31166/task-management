d2110f2426d73a610e5db56a4b94918d6a47e441
Tien <tranminhtien31166@gmail.com>
2022-12-11 22:59:00 +0700

    [fix] critial bug

--- src/app/store/reducers/card.reducer.ts
+++ src/app/store/reducers/card.reducer.ts
@@ -1,8 +1,6 @@
 import { CardAction } from '../actions/card.actions';
 import { ModelCard } from '@app/models';
-import { stringify } from 'querystring';
 import _findIndex from 'lodash/findIndex';
-import _cloneDeep from 'lodash/cloneDeep';
 
 export function setCardsFromLocalStore(cards) {
   localStorage.setItem('CARDS', JSON.stringify(cards))
@@ -18,7 +16,7 @@ export function CardReducer(
       setCardsFromLocalStore([...state, action.payload]);
       return [...state, action.payload];
     case 'UPDATE_CARD_BY_ID':
-      let stateParse = JSON.parse(JSON.stringify(_cloneDeep(state)));
+      let stateParse = JSON.parse(JSON.stringify(state));
       let index = _findIndex(stateParse, function (o) { return o.id == action.payload.id; });
       stateParse[index] = action.payload;
       setCardsFromLocalStore(stateParse);
